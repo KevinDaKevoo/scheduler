92c1c12dbb103f067403daa19fd379dd58085bf4
Kevin <kevin.yu.kfy@gmail.com>
2021-07-07 19:30:18 -0400

    Initial Commit

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,79 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import DayList from "components/DayList";
 import "components/Application.scss";
+import Appointment from "components/Appointment";
+const axios = require("axios");
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Kevin",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  }
+];
+
+const formatedAppointments = appointments.map((appointment) => {
+  return (
+    <Appointment
+      key={appointment.id}
+      {...appointment}
+    />
+  );
+});
+
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
+  useEffect(()=>{
+    axios.get('/api/days')
+    .then(response => setDays(response.data));
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+      <img
+  className="sidebar--centered"
+  src="images/logo.png"
+  alt="Interview Scheduler"
+/>
+<hr className="sidebar__separator sidebar--centered" />
+<nav className="sidebar__menu"><DayList
+  days={days}
+  day={"Monday"}
+  setDay={day => console.log(day)}
+/></nav>
+<img
+  className="sidebar__lhl sidebar--centered"
+  src="images/lhl.png"
+  alt="Lighthouse Labs"
+/>
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {formatedAppointments}
+        
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Button from "../Button"
+import InterviewerList from "../InterviewerList"
+
+export default function From(props) {
+
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  const validate = function() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    } 
+    else if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError('');
+    props.onSave(name, interviewer)
+  };
+
+  const reset = function() {
+    setError(null);
+    setName('');
+    setInterviewer(null);
+    props.onCancel();
+  };
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => {setName(event.target.value)}}
+            value={name || props.name || ""}
+            data-testid="student-name-input"
+          />
+        </form>
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList interviewers={props.interviewers} setInterviewer={setInterviewer} interviewer={interviewer || props.interviewer} />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={reset} danger>Cancel</Button>
+          <Button onClick={validate} confirm>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+};
